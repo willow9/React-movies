4c86591f4df456a4bf3902ad9db7c4cf51600202
willow <jurate.baltrukenaite@gmail.com>
2020-02-13 23:57:27 +0200

    Started to create components and templates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
 
-function App() {
-  return (
-    <div >
-      
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Dashboard}></Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className='nav-wrapper #927b7c'>
+      <div className='container'>
+        <Link to='/' className='brand-logo'>
+          WTW
+        </Link>
+        <ul className='right'>
+          <li>
+            <a>Login</a>
+          </li>
+          <li>
+            <a>SignUp</a>
+          </li>
+          <li>
+            <a>Search</a>
+          </li>
+          <li>
+            <a>LogOut</a>
+          </li>
+          <li>
+            <i class='material-icons'>close</i>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
