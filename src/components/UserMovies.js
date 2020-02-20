ea0df4b7e06263155abaa730a7622f6213b9bd46
willow <jurate.baltrukenaite@gmail.com>
2020-02-20 20:56:22 +0200

    fixed overflowing card with movie poster

--- src/components/UserMovies.js
+++ src/components/UserMovies.js
@@ -1,29 +1,33 @@
-import React from 'react';
+import React, { useEffect, Component } from 'react';
 import picture from '../images/logo512.png';
 import { connect } from 'react-redux';
-import {fetchMovies} from "../redux"
+import { fetchMovies } from '../redux';
 
 const UserMovies = props => {
+
+  // useEffect(() => {
+  //   props.fetchMovies();
+  // }, []);
+
   console.log(props);
-  
-  const onClick = (e)=>{
-    props.fetchMovies()
-    
-  }
+
+  const onClick = e => {
+    props.fetchMovies();
+  };
+
   return (
     <div className='col s12 m8 l8  grey'>
       <h3 className='center'>Favorite Movies</h3>
       <div className='row'>
         {props.movies.map(el => {
           return (
-            <div className='col s12 m6 l3' key= {el.id}>
+            <div className='col s12 m6 l3' key={el.id}>
               <div className='card '>
                 <div className='card-image'>
                   <img src={el.Poster} alt={picture} />
-                 
                 </div>
                 <div className='card-content'>
-                  <h6>{el.Title}</h6>
+                  <h6>{el.title}</h6>
                   <p>genre</p>
                   <p>released</p>
                   <p>rating</p>
@@ -33,7 +37,7 @@ const UserMovies = props => {
           );
         })}
       </div>
-      <button onClick = {() => onClick()}>Test</button>
+      <button onClick={() => onClick()}>Test</button>
       <div className='row orange'>
         <div className='col s12  m2'>3</div>
         <div className='col s12  m2'>2</div>
@@ -56,4 +60,4 @@ const mapStateToProps = state => {
     movies: state.movies
   };
 };
-export default connect(mapStateToProps, {fetchMovies})(UserMovies);
+export default connect(mapStateToProps, { fetchMovies })(UserMovies);
