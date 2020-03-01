0feb7a15652a48c59ea47baef0314846c1d996e1
willow <jurate.baltrukenaite@gmail.com>
2020-03-01 17:02:42 +0200

    experimenting with portalmodal2

--- src/pages/TestingModal.js
+++ src/pages/TestingModal.js
@@ -10,7 +10,7 @@ class UserMovies extends Component {
   //   useEffect(() => {
   //     fetchMovies();
   //   }, [fetchMovies]);
-  //   const { fetchMovies, movies, title } = this.props;
+  //  const { fetchMovies, movies, title } = this.props;
   state = {
     isOpen: false,
     movieIndex: null
@@ -34,12 +34,35 @@ class UserMovies extends Component {
         <React.Fragment>
           <ModalContent>
             {this.state.isOpen ? (
-              <div className='modalas-background'>
-                {/* <div className='modalas-content'>
-                  portal is opening now... {this.props.movies[this.state.movieIndex].Title}
-                  <button onClick={this.toggle}>Close</button>
-                  <div className='col l3 card-container'> */}
-                    <div className='col s12 m6 l1' key={this.state.movieIndex}>
+              <div className=' modalas-background'>
+                <div className=' container row col s12 modalas-content'>
+                  {/* portal is opening now... {this.props.movies[this.state.movieIndex].Title}
+                  <button onClick={this.toggle}>Close</button> */}
+                  {/* <div className='col l3 card-container'> */}
+
+                  {/* <div className='col s12 m6 l1' key={this.state.movieIndex}>
+                      <div className='card '>
+                        <div className='card-image'>
+                          <img src={this.props.movies[this.state.movieIndex].Poster} alt={picture} />
+                        </div>
+                        <div className='card-content'>
+                          <h6>{this.props.movies[this.state.movieIndex].Title}</h6>
+                          <hr />
+                          <p>IMDB ID: {this.props.movies[this.state.movieIndex].imdbID}</p>
+                          <p>{this.props.movies[this.state.movieIndex].Year} year</p>
+                        </div>
+                        <button
+                          onClick={this.toggle}
+                          className='btn waves-effect waves-light commend'
+                          id={this.state.movieIndex}
+                        >
+                          Close
+                        </button>
+                      </div>
+                    </div> */}
+
+                  <div className='col s12 m4 l4 card-container1'>
+                    <div className='col s12 m12 l12' key={this.state.movieIndex}>
                       <div className='card '>
                         <div className='card-image'>
                           <img src={this.props.movies[this.state.movieIndex].Poster} alt={picture} />
@@ -59,10 +82,24 @@ class UserMovies extends Component {
                         </button>
                       </div>
                     </div>
-                    <div className='col s6 m6 l1 comment'>here will be comment form</div>
                   </div>
-              //   </div>
-              // </div>
+                  <div className='col s12 m8 l8 card-container1'>
+                    <div class='row'>
+                      <div class='input-field col s12'>
+                        <textarea id='textarea1' class='materialize-textarea' rows='3'></textarea>
+                        <label for='textarea1'>Anotation</label>
+                      </div>
+                    </div>
+                    <div class='row'>
+                      <div class='input-field col s12'>
+                        <textarea id='textarea2' class='materialize-textarea' rows='3'></textarea>
+                        <label for='textarea2'>Rate</label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* //   </div> */}
+              </div>
             ) : null}
           </ModalContent>
         </React.Fragment>
@@ -71,7 +108,7 @@ class UserMovies extends Component {
           {this.props.title && <h3 className='center'>Favorite Movies</h3>}
           <div className='row'>
             {this.props.movies ? (
-              this.props.movies.slice(0, 2).map((el, index) => {
+              this.props.movies.slice(0, 8).map((el, index) => {
                 return (
                   <div className='col s12 m6 l3' key={index}>
                     <div className='card '>
