4538ae9c1dfece8e7706a360b05bae151719be1b
willow <jurate.baltrukenaite@gmail.com>
2020-03-04 23:18:26 +0200

    started the video card style for the modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -7,9 +7,9 @@ class Modal1 extends Component {
   componentWillMount() {
     this.props.fetchMovie(this.props.imdbId);
   }
-  toggle = () =>{
-    this.props.toggle()
-  }
+  toggle = () => {
+    this.props.toggle();
+  };
   render() {
     console.log('is modali=');
 
@@ -17,26 +17,75 @@ class Modal1 extends Component {
 
     return (
       <div className='modalas-background'>
-        <div class='card'>
-          <div>
-            <h1>Hello from portal</h1>
-            <h3>{this.props.movie.Plot}</h3>
-            <button onClick = {this.toggle}>Untoggle</button>
+        {/* <div className='container green'> */}
+
+        {/* TITLE */}
+
+        <div className='card'>
+          <div className='row'>
+            <div className=' center-align grey'>
+              <h1>{this.props.movie.Title}</h1>
+              <button onClick={this.toggle}>Untoggle</button>
+            </div>
+          </div>
+          {/* OUTER CARD CONTENT START*/}
+
+          <div className='row blue outerDiv'>
+            <div className='col l6 m6 s12 image-placeholder center-align'>
+              <img src={this.props.movie.Poster} />
+            </div>
+            <div className='col l6 m6 s12 red'>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>Genre:</td>
+                    <td>{this.props.movie.Genre}</td>
+                  </tr>
+
+                  <tr>
+                    <td>Year: </td>
+                    <td>{this.props.movie.Year}</td>
+                  </tr>
+                  <tr>
+                    <td>Runtime:</td>
+                    <td>{this.props.movie.Runtime}</td>
+                  </tr>
+                  <tr>
+                    <td>Actors:</td>
+                    <td>{this.props.movie.Actors}</td>
+                  </tr>
+                  <tr>
+                    <td>Director:</td>
+                    <td>{this.props.movie.Director}</td>
+                  </tr>
+                  <tr>
+                    <td>Writer:</td>
+                    <td>{this.props.movie.Writer}</td>
+                  </tr>
+                  <tr>
+                    <td>Awards:</td>
+                    <td>{this.props.movie.Awards}</td>
+                  </tr>
+                  <tr>
+                    <td>IMDB Rating:</td>
+                    <td>{this.props.movie.imdbRating}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
 
-          <div class='card-image waves-effect waves-block waves-light'>
-            <img class='activator' src={this.props.movie.Poster} />
+          <div className='card-content'>
+            <span className='card-title activator grey-text text-darken-4'>PlotOuter</span>
+            {/* </div> */}
           </div>
-          <div class='card-content'>
-            <span class='card-title activator grey-text text-darken-4'>
-              Card Title<i class='material-icons right'>more_vert</i>
+
+          {/* INACTIVE CARD CONTENT */}
+          <div className='card-reveal'>
+            <span className='card-title grey-text text-darken-4'>
+              Plot<i className='material-icons right'>close</i>
             </span>
-          </div>
-          <div class='card-reveal'>
-            <span class='card-title grey-text text-darken-4'>
-              Card Title<i class='material-icons right'>close</i>
-            </span>
-            <p>Here is some more information about this product that is only revealed once clicked on.</p>
+            <p>{this.props.movie.Plot}</p>
           </div>
         </div>
       </div>
