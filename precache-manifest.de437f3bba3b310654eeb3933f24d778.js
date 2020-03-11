e5f3f3c4f7d826d0e094ba28e5ad78c48c013408
willow <jurate.baltrukenaite@gmail.com>
2020-03-11 22:52:29 +0200

    Updates

---
+++ precache-manifest.de437f3bba3b310654eeb3933f24d778.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea36a6e121304eab39f766ac611e9444",
+    "url": "/React-movies/index.html"
+  },
+  {
+    "revision": "2d0d3f85b2250758322a",
+    "url": "/React-movies/static/css/main.d083399a.chunk.css"
+  },
+  {
+    "revision": "5cd68717b8e7aa18a16a",
+    "url": "/React-movies/static/js/2.695b053a.chunk.js"
+  },
+  {
+    "revision": "437712da562e65012de83965d4105c97",
+    "url": "/React-movies/static/js/2.695b053a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d0d3f85b2250758322a",
+    "url": "/React-movies/static/js/main.2682f58a.chunk.js"
+  },
+  {
+    "revision": "7ac41f86dfde64c0ec15",
+    "url": "/React-movies/static/js/runtime-main.ff2e83f5.js"
+  },
+  {
+    "revision": "36e49f7f4ee417ff81f7cbff675e5992",
+    "url": "/React-movies/static/media/animals.36e49f7f.jpg"
+  },
+  {
+    "revision": "7ee3931e22adc1781751b97776887795",
+    "url": "/React-movies/static/media/noPicture.7ee3931e.jpg"
+  }
+]);
