196ee428c72272e0b0cf80b22a92ec3061dc1126
willow <jurate.baltrukenaite@gmail.com>
2020-03-12 23:29:07 +0200

    Improved user profile component style.

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import picture from '../images/animals.jpg';
+import noPicture from '../images/noPicture.jpg';
 
 const UserProfile = ({ user }) => {
   return (
-    <div className='col s12 m4 l4 green'>
-      <div className='valign-wrapper'>
-        <div className='card large'>
-          <div className='card-image circle'>
-            <img src={picture} alt='profile' />
-          </div>
-          <div className='card-content'>
-            <h3>
-              {user.firstName} {user.lastName}
-            </h3>
-            <p>
-              I am a very simple card. I am good at containing small bits of information. I am convenient because I
-              require little markup to use effectively.
-            </p>
-          </div>
-        </div>
+    <div className='col s12 m4 l4 userProfileWrapper'>
+      <div className='valign-wrapper userProfile'>
+        <img className='z-depth-5' src={noPicture} alt='user' />
+        <h4 className='center-align'>
+          {user.firstName} {user.lastName}
+        </h4>
       </div>
     </div>
   );
